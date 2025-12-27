58ee56d99fea0309cb1a7f79c92aef6ee27559b5
Tushar Jethva <tusharjethva90@gmail.com>
2025-12-27 14:23:30 +0530

    add implement

--- Tj.java
+++ Tj.java
@@ -2,7 +2,7 @@ class Addition
 {
     public static void main(String[] args) {
         int a = 10;
-        int b = 50;
+        int b = 30;
         int sum = a + b;
 
         System.out.println("Sum = "+ sum);
